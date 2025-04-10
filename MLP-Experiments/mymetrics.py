353371e7e1ae5539f68742d7cb9e7794a7cf13c8
Francisco Mateus <franciscomateus0119@gmail.com>
2025-04-10 10:26:13 -0300

    Results

--- MLP-Experiments/mymetrics.py
+++ MLP-Experiments/mymetrics.py
@@ -11,3 +11,14 @@ def calculate_coverage(dataset_df, l):
         df_copy = df_copy[(feature[0] <= df_copy[df_copy.columns[i]]) &  (df_copy[df_copy.columns[i]] <= feature[1])]
     #display(df_copy)
     return df_copy
+
+def calculate_coverage_with_tolerance(dataset_df, l, tolerance=1e-7):
+    df_copy = dataset_df.copy()
+    bounds = np.array(l) 
+    for i, (lower_bound, upper_bound) in enumerate(bounds):
+        column = df_copy.columns[i]
+        df_copy = df_copy[
+            (df_copy[column] >= (lower_bound - tolerance)) & 
+            (df_copy[column] <= (upper_bound + tolerance))
+        ]
+    return df_copy
